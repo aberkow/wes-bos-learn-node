370655ad73fa46621fbfdba1c619824232906c1f
aberkow <berkowitz.clarinet@gmail.com>
2017-06-01 21:58:37 -0400

    working through

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -4,6 +4,11 @@ const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
   console.log(req.name);
+  // flash() takes two arguments. the type and the content to render out.
+  // req.flash('error', 'Something bad happened');
+  // req.flash('info', 'Something happened');
+  // req.flash('warning', 'Something questionable happened');
+  // req.flash('success', 'Something good happened');
   res.render('index');
 }
 
@@ -19,7 +24,15 @@ exports.addStore = (req, res) => {
 // when the function is marked async node/browser knows that there will be async await code.
 exports.createStore = async (req, res) => {
   //res.json(req.body); // req.body contains all the information taht was sent.
-  const store = new Store(req.body);
+  // the slug is auto generated so you have to await the result of saving the store.
+  const store = await (new Store(req.body)).save();
   await store.save() // saves data to db. or throws an error.
-  res.redirect('/');
+  req.flash('sucess', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async (req, res) => {
+  // 1 - query the db for a list of stores.
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
 }
